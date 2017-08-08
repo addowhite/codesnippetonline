54ecd9c683c30283e021b0d0e39f2414f2de92ca
Adorjan White <addowhite@gmail.com>
2017-08-08 23:05:48 +0100

    Added alert when not signed in and attempting to like/dislike snippets

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,11 @@
 function updateSnippetLikes(snippet_id, response) {
-    var like_dislike_counts = response.split("|");
-    document.getElementById("like_snippet_count_" + snippet_id).innerHTML = like_dislike_counts[0];
-    document.getElementById("dislike_snippet_count_" + snippet_id).innerHTML = like_dislike_counts[1];
+    if (response === "") {
+        alert("You must be signed in to like or dislike snippets.");
+    } else {
+        var like_dislike_counts = response.split("|");
+        document.getElementById("like_snippet_count_" + snippet_id).innerHTML = like_dislike_counts[0];
+        document.getElementById("dislike_snippet_count_" + snippet_id).innerHTML = like_dislike_counts[1];
+    }
 }
 
 function addLikeButtonCallback(like_dislike, snippet_id) {
